b155aaf5ea021d3e7b9a7dc1e0c7ab3910dd0442
jhingx1 <jhingx1@gmail.com>
2018-09-28 16:31:05 -0500

    URLs dinamicos basicos.

--- cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
+++ cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,9 +37,13 @@ public class HomeController {
 
 		return "home";
 	}
-
-	@RequestMapping(value = "/detail")
-	public String mostrarDetalle(Model model) {
+	
+	//url dinamicos
+	@RequestMapping(value = "/detail/{id}",method=RequestMethod.GET)
+	public String mostrarDetalle(Model model,@PathVariable("id") int idPelicula) {
+		
+		System.out.println("idPelicula : " + idPelicula);
+		
 		String tituloPelicula = "Rapidos y furiosos";
 		int duracion = 136;
 		double precioEntrada = 50;
