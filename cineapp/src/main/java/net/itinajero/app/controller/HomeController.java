0fb9352ef90a5581b1c2dba19e53a7359a1e4c2d
jhingx1 <jhingx1@gmail.com>
2018-09-28 17:05:39 -0500

    Paso de parametros. RequestParam - GET

--- cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
+++ cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.itinajero.app.model.Pelicula;
 
@@ -48,8 +49,10 @@ public class HomeController {
 	}
 	
 	//url dinamicos
-	@RequestMapping(value = "/detail/{id}/{fecha}",method=RequestMethod.GET)
-	public String mostrarDetalle(Model model,@PathVariable("id") int idPelicula,@PathVariable("fecha") String fecha) {
+	//@RequestMapping(value = "/detail/{id}/{fecha}",method=RequestMethod.GET)
+	@RequestMapping(value = "/detail",method=RequestMethod.GET)
+	//public String mostrarDetalle(Model model,@PathVariable("id") int idPelicula,@PathVariable("fecha") String fecha) {
+	public String mostrarDetalle(Model model,@RequestParam("idMovie") int idPelicula,@RequestParam("fecha") String fecha) {	
 		
 		System.out.println("Horarios Pelicula : " + idPelicula);
 		System.out.println("Fecha  : " + fecha);
