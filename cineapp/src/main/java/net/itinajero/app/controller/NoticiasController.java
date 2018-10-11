0a4891cb838df4fa630b187aacf4d317e50bd6b2
jhingx1 <jhingx1@gmail.com>
2018-10-10 22:56:11 -0500

    Model para la clase Noticia.

--- cineapp/src/main/java/net/itinajero/app/controller/NoticiasController.java
+++ cineapp/src/main/java/net/itinajero/app/controller/NoticiasController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.itinajero.app.model.Noticia;
+
 @Controller
 @RequestMapping(value="/noticias")
 public class NoticiasController {
@@ -22,6 +24,15 @@ public class NoticiasController {
 			@RequestParam("estatus") String estatus,
 			@RequestParam("detalle") String detalle
 			) {
+		//Tenemos toda la informacion del formulario en un objeto creado.
+		Noticia noticia = new Noticia();
+		noticia.setTitulo(titulo);
+		noticia.setEstatus(estatus);
+		noticia.setDetalle(detalle);
+		
+		//Guardar el objeto noticia en la BD.
+		
+		System.out.println(noticia);
 		
 			System.out.println("Titulo : " + titulo + " Estatus: " + estatus + " detalle:" +detalle);
 		
