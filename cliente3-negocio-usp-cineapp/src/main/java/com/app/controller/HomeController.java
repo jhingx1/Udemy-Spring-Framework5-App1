0be1b7aa6fa6b6822ea6e46d505ded28422d7a85
jhingx1 <jhingx1@gmail.com>
2019-09-22 13:16:01 -0500

    Uso de JSTL

--- cliente3-negocio-usp-cineapp/src/main/java/com/app/controller/HomeController.java
+++ cliente3-negocio-usp-cineapp/src/main/java/com/app/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.app.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,4 +18,38 @@ public class HomeController {
 		return "home";
 		
 	}
+	
+	@RequestMapping(value="/detail")
+	public String mostrarDetalle(Model model) {
+		String tituloPelicula = "Rapidos y Furiosos";
+		int duracion = 136;
+		double precioEntrada = 50;
+		
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precioEntrada", precioEntrada);
+		
+		//regresando el nombre de la vista
+		return "detalle";
+		
+	}
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String mostrarPrincipal(Model model) {
+		
+		List<String> peliculas = new LinkedList<>();
+		peliculas.add("Rapidos y Furiosos");
+		peliculas.add("El aro 2");
+		peliculas.add("Aliens");
+		model.addAttribute("peliculas", peliculas);
+		
+		String demo = "demo";
+		model.addAttribute("demo", demo);
+		
+		
+		return "home";
+	}
+	
+	
+	
 }
--- cliente3-negocio-usp-cineapp/webapp/WEB-INF/views/detalle.jsp
+++ cliente3-negocio-usp-cineapp/webapp/WEB-INF/views/detalle.jsp
@@ -1,24 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-   
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Bienbenido a CineApp</title>
+<title>Detalle Pelicula</title>
 </head>
 <body>
-	<h1>Lista de Peliculas</h1>
-	
-	<ol>
-		<c:forEach items="${peliculas}" var="pelicula" >
-			<li>${pelicula}</li>
-		 </c:forEach>
-	</ol>
-	
-	<p>${peliculas}</p>
-	<p>demo : ${demo}</p>
-	
+	<h1>Titulo de la Pelicula ${titulo}</h1>
+	<h2>Duracion ${duracion}</h2>
+	<h2>Precio ${precioEntrada}</h2>
 </body>
 </html>
